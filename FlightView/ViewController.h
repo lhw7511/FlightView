17f45c981cac11213aba51445fd90f3c9ed9c504
hyunwooklee <lhw751@naver.com>
2022-04-28 14:41:38 +0900

    setting value event

--- FlightView/ViewController.h
+++ FlightView/ViewController.h
@@ -7,12 +7,17 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController<UITextFieldDelegate>{
+    NSInteger buttonTag;
+}
 @property (strong, nonatomic) IBOutlet UILabel *returnDateLabel;
 @property (strong, nonatomic) IBOutlet UIButton *returnDateButton;
 @property (strong, nonatomic) IBOutlet UIDatePicker *selectDatePicker;
 - (IBAction)showReturnDateAction:(id)sender;
+- (IBAction)showDatePickerAction:(id)sender;
+- (IBAction)selectDateAction:(id)sender;
 
+@property (strong, nonatomic) IBOutlet UIButton *departureDateButton;
 
 @end
 
--- FlightView/ViewController.m
+++ FlightView/ViewController.m
@@ -12,9 +12,10 @@
 @end
 
 @implementation ViewController
-@synthesize returnDateLabel,returnDateButton,selectDatePicker;
+@synthesize returnDateLabel,returnDateButton,selectDatePicker,departureDateButton;
 - (void)viewDidLoad {
     [super viewDidLoad];
+    buttonTag = 0;
     returnDateLabel.hidden = true;
     returnDateButton.hidden = true;
     selectDatePicker.hidden = true;
@@ -22,14 +23,35 @@
 
 
 
-- (IBAction)showReturnDateAction:(id)sender {
-    if(returnDateLabel.hidden){
-        returnDateLabel.hidden = false;
-        returnDateButton.hidden = false;
+- (IBAction)selectDateAction:(id)sender {
+    NSDateFormatter *formatter = [[NSDateFormatter alloc] init];
+    formatter.dateFormat = @"YY-MM-dd hh:mma";
+    NSString *dateString = [formatter stringFromDate:selectDatePicker.date];
+    if(buttonTag == 1){
+        [departureDateButton setTitle:dateString forState:UIControlStateNormal];
     }else{
-        returnDateLabel.hidden = true;
-        returnDateButton.hidden = true;
+        [returnDateButton setTitle:dateString forState:UIControlStateNormal];
     }
     
+}
+
+- (IBAction)showDatePickerAction:(id)sender {
+    selectDatePicker.hidden = false;
+    buttonTag = [sender tag];
+}
+
+- (IBAction)showReturnDateAction:(id)sender {
+    returnDateLabel.hidden = ![sender isOn];
+    returnDateButton.hidden = ![sender isOn];
+    
+}
+
+- (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event{
+    [self.view endEditing:YES];
+    selectDatePicker.hidden = true;
+}
+- (BOOL)textFieldShouldReturn:(UITextField *)textField{
+    [textField resignFirstResponder];
+    return YES;
 }
 @end
